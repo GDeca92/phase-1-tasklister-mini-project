fdadf9f3f1a8d0de63e00173effc489d5cfccd47
Gian Carlo De Castro <Giancld92@gmail.com>
2022-09-08 22:32:46 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form =document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    // console.log(e.target.new_task_description)
+    listToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function listToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  li.textContent = `${todo} ` 
+  btn.textContent = 'X'
+  btn.addEventListener("click" , deleteBtn)
+  li.appendChild(btn)
+  document.getElementById('tasks').appendChild(li)
+}
+
+function deleteBtn(e) {
+  e.target.parentNode.remove() 
+}
